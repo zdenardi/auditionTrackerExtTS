5b37b880574af45b517fe87892171532d684083f
zachdenardi <zdenardi@gmail.com>
2025-01-19 13:04:21 -0500

    removed uneeded import

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,4 +1,3 @@
-import { SrvRecord } from "dns";
 import { sendAuditionToSpreadsheet, doesFileExists } from "./googleapi";
 export const SHEET_NAME = "***Audition Tracker***";
 export const TEMPLATE_ID = "1C-lv_NFOQZ0BQnTvrBqhpCZaDDUZg_8I9zmI_e8DbYk";
